77348098a528e936ec57c127aaf00da06f6c7649
Fabian Kreßin <61588952+fabiankressin@users.noreply.github.com>
2024-01-17 16:42:42 +0100

    added enemy attacks

--- Assets/Scripts/Enemy.gd
+++ Assets/Scripts/Enemy.gd
@@ -1,21 +1,20 @@
 extends RigidBody3D
 
-# Exposed variables
+# variables
 var move_speed : float = .01
-var detection_radius : float = 8.0
-var attack_radius : float = 5.0
+var detection_radius : float = 5.0
+var attack_radius : float = 2.0
 var attack_damage : int = 20
 var attack_cooldown : float = 3.0
 
-# Private variables
 var game_manager : GameManager
 var player : Player
-var last_attack_time : float
+var time_since_last_attack : float = 0.0
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
-	player = get_node("/root/GameScene/Player") # Assuming player is a top-level node named "Player"
+	player = get_node("/root/GameScene/Player")
 	game_manager = get_node("/root/GameScene/GameManager")
 	
 	if player == null:
@@ -27,19 +26,22 @@ func _ready():
 		set_process(false)
 
 func _process(delta):
-	print(global_transform.origin.distance_to(player.global_transform.origin))
-	if player != null and global_transform.origin.distance_to(player.global_transform.origin) <= detection_radius:
+	if player == null:
+		return
+	var dist = global_transform.origin.distance_to(player.global_transform.origin)
+	if dist <= detection_radius:
 		move_towards_player()
-		attack_player()
+	if dist <= attack_radius:
+		attack_player(delta)
 
 func move_towards_player():
-	# Orient the enemy towards the player
 	look_at(player.global_transform.origin, Vector3(0, 1, 0))
-	
-	# Move the enemy in the forward direction
 	translate(Vector3.FORWARD * move_speed)
 
-func attack_player():
-	pass
-	# Assuming GameManager is a singleton and DamagePlayer is a function in that singleton
-	#game_manager.damage_player(attack_damage)
+func attack_player(delta):
+	if time_since_last_attack >= attack_cooldown:
+		game_manager.DamagePlayer(attack_damage)
+		time_since_last_attack = 0.0
+	else:
+		time_since_last_attack += delta
+		
