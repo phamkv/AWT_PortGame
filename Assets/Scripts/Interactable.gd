33fa06ba202d2199bf9693cc1135628048f020a5
Fabian Kreßin <61588952+fabiankressin@users.noreply.github.com>
2024-01-20 19:21:31 +0100

    dropping multiple stones now

--- Assets/Scripts/Interactable.gd
+++ Assets/Scripts/Interactable.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @export var replacementObject: PackedScene
+@export var dropCount: int = 1
 
 var health: int = 5
 
@@ -15,11 +16,16 @@ func _process(delta):
 	
 
 func destroy_object():
-	var replacementInstance = replacementObject.instantiate()
-	get_parent().add_child(replacementInstance)
-	
-	replacementInstance.transform.origin = transform.origin
-
+	for i in range(dropCount):
+		var replacementInstance = replacementObject.instantiate()
+		get_parent().add_child(replacementInstance)
+		
+		var randomPosition = Vector3(
+			randf_range(-0.5, 0.5),
+			randf_range(0.0, 2.5),
+			randf_range(-0.5, 0.5)
+		)
+		replacementInstance.transform.origin = transform.origin + randomPosition
 	queue_free()
 
 func interaction() -> void:
